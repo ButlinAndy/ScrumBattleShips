d0c716d744986fe86aab1707a0d51110b1c68c09
Paul Lancashire <plancashire@anglianwater.co.uk>
2022-01-06 13:00:23 +0000

    fix(test): remove unneeded test

--- Battleship_Tests/parsePositionTests.js
+++ Battleship_Tests/parsePositionTests.js
@@ -10,9 +10,3 @@ describe('parsePositionTests', function() {
     assert.deepStrictEqual(actual, expected);
   });
 });
-
-describe('messageColourTests', function(){
-    it('should use yellow text for computer messages', function(){
-        var expected = new cliColor.red();
-    })
-})
